cfcfd9e5c3b71c1e7c0f2540854cf2308011ab13
HtunLinn <htunlinn@mm.cybermissions.co.jp>
2022-03-18 18:32:45 +0900

    date time format

--- plugins/datetime_format.js
+++ plugins/datetime_format.js
@@ -3,5 +3,5 @@ import moment from 'moment'
 
 Vue.filter('datetime', function (value) {
   const datetime = moment(value)
-  return moment(datetime).format('YYYY年MM月DD日')
+  return moment(datetime).format('YYYY年MM月DD日 HH時mm分')
 })
